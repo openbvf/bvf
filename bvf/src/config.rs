040809be1daf4f45662df7351560570e60634624
uncertaininference <gituc@newvoll.net>
2026-04-19 12:20:51 -0700

    Restrict internal constants to pub(crate)

--- bvf/src/config.rs
+++ bvf/src/config.rs
@@ -1,9 +1,9 @@
 use libsodium_rs::crypto_scalarmult::BYTES as X25519_BYTES;
 use libsodium_rs::crypto_secretstream::xchacha20poly1305::HEADERBYTES as SS_HEADERBYTES;
 
-pub const VERSION_HEADER: &[u8; 7] = b"bvf-v1\n";
-pub const HPKE_INFO: &[u8] = b"bvf.hpke.x25519-sha256-exportonly";
-pub const HPKE_EXPORT_CONTEXT: &[u8] = b"bvf-master";
+pub(crate) const VERSION_HEADER: &[u8; 7] = b"bvf-v1\n";
+pub(crate) const HPKE_INFO: &[u8] = b"bvf.hpke.x25519-sha256-exportonly";
+pub(crate) const HPKE_EXPORT_CONTEXT: &[u8] = b"bvf-master";
 /// Plaintext chunk size
 pub const CHUNK_SIZE: usize = 64 * 1024;
 
